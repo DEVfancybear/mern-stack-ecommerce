2f25cf590af35dfdd7f2015c33c79c88a75f5582
DEVfancybear <duonghacker2345@gmail.com>
2020-05-05 09:14:13 +0700

    create main index.js server

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+// const connectDB = require("./config/db");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+//Connect database
+// connectDB();
+app.get("/", (req, res) => res.send("Welcome to database"));
+// Unlock all cors
+app.use(cors());
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//Define Routes
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")));
+    //
+    app.get("*", (req, res) => {
+        res.sendfile(path.join((__dirname = "client/build/index.html")));
+    });
+}
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

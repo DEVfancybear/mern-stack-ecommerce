bce4a3f85d250ef0880d9030902ef5ed23b38fc4
DEVfancybear <duonghacker2345@gmail.com>
2020-05-06 10:47:50 +0700

    define routes

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //Define Routes
-
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/products", require("./routes/api/products"));
+app.use("/api/profile", require("./routes/api/profile"));
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client/build")));
     //

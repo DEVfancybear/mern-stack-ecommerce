d968770d2be749acc1f0bbef518fe8b7d23083c7
DEVfancybear <duonghacker2345@gmail.com>
2020-05-06 10:39:01 +0700

    create model Product

---
+++ models/Product.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const ProductSchema = new mongoose.Schema({
+    writer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    title: {
+        type: String,
+        maxlength: 50
+    },
+    description: {
+        type: String
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    images: {
+        type: Array,
+        default: []
+    },
+    continents: {
+        type: Number,
+        default: 1
+    },
+    sold: {
+        type: Number,
+        maxlength: 100,
+        default: 0
+    },
+    views: {
+        type: Number,
+        default: 0
+    }
+}, { timestamps: true })
+module.exports = Product = mongoose.model('product', ProductSchema);

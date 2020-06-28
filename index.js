cf4fc81368377a1e0a94c732f4d493355a3d054c
DuongLT <duonghacker2345@gmail.com>
2020-06-28 21:38:37 +0700

    update root server nack-end

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const path = require("path");
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import config from "./config/config";
+import userRoute from "./routes/api/userRoute";
+import productRoute from "./routes/api/productRoute";
+import orderRoute from "./routes/api/orderRoute";
+import uploadRoute from "./routes/api/uploadRoute";
+import cors from "cors";
+const mongodbUrl = config.MONGODB_URL;
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch((error) => console.log(error.reason));
+
 const app = express();
-//Connect database
-connectDB();
-app.get("/", (req, res) => res.send("Welcome to database"));
-// Unlock all cors
-app.use(cors());
-// Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//Define Routes
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-// app.use("/api/products", require("./routes/api/products"));
-// app.use("/api/profile", require("./routes/api/profile"));
+app.use(cors());
+app.use("/api/uploads", uploadRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(config.PAYPAL_CLIENT_ID);
+});
+app.use("/uploads", express.static(path.join(__dirname, "/../uploads")));
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
-    //
-    app.get("*", (req, res) => {
-        res.sendfile(path.join((__dirname = "client/build/index.html")));
-    });
+  app.use(express.static(path.join(__dirname, "client/build")));
+  //
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
+  });
 }
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.listen(config.PORT, () => {
+  console.log("Server started at http://localhost:5000");
+});

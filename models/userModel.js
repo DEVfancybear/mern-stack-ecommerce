a72f0a695ea0063585904ff7eb4ec5f6291a161f
DuongLT <duonghacker2345@gmail.com>
2020-06-28 21:44:26 +0700

    fix userModal

--- models/userModel.js
+++ models/userModel.js
@@ -1,25 +1,18 @@
-const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    avatar: {
-        type: String
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
+import mongoose from "mongoose";
 
-module.exports = User = mongoose.model('user', UserSchema);
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+    dropDups: true,
+  },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, required: true, default: false },
+});
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;

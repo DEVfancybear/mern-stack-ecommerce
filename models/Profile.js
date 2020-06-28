5798a5b53591a3e0e4a37fd08bc6b3b3b90e8e08
DuongLT <duonghacker2345@gmail.com>
2020-06-28 21:09:14 +0700

    models new Profile

--- models/Profile.js
+++ models/Profile.js
@@ -1,27 +1,13 @@
 const mongoose = require('mongoose');
 
+
 const ProfileSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
-    },
-    role: {
-        type: Number,
-        default: 0
-    },
-    cart: {
-        type: Array,
-        default: []
-    },
-    history: {
-        type: Array,
-        default: []
-    },
-    image: String,
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  name: { type: String, required: true },
+  email: {
+    type: String, required: true, unique: true, index: true, dropDups: true,
+  },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, required: true, default: false },
 });
 
 module.exports = Profile = mongoose.model('profile', ProfileSchema);

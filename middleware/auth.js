ffa88d70583fabb3fd8d34ba8395c994e25d1959
DuongLT <duonghacker2345@gmail.com>
2020-06-28 21:13:26 +0700

    update middlewares

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,44 @@
-const jwt = require("jsonwebtoken");
-const config = require("config");
-module.exports = function (req, res, next) {
-//Get token from header
-    const token = req.header("x-auth-token");
-    // Check if not token
-    if (!token) {
-        return res.status(401).json({msg: "No token , authorization denied"})
+import jwt from 'jsonwebtoken';
+import config from './config';
+const getToken = (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    config.JWT_SECRET,
+    {
+      expiresIn: '48h',
     }
-    // Verify token
-    try {
-        const decoded = jwt.verify(token, config.get("jwtSecret"));
-        req.user = decoded.user;
-        next();
-    } catch (e) {
-        res.status(401).json({msg: "Token is not valid"});
-    }
-}
+  );
+};
+
+const isAuth = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    const onlyToken = token.slice(7, token.length);
+    jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res.status(401).send({ message: 'Invalid Token' });
+      }
+      req.user = decode;
+      next();
+      return;
+    });
+  } else {
+    return res.status(401).send({ message: 'Token is not supplied.' });
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  console.log(req.user);
+  if (req.user && req.user.isAdmin) {
+    return next();
+  }
+  return res.status(401).send({ message: 'Admin Token is not valid.' });
+};
+
+export { getToken, isAuth, isAdmin };
